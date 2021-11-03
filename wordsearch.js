bfd43de417e2101b2954c1f5ddd6f22cc27b780d
satya sindhu <movva001@gmail.com>
2021-11-03 17:03:44 -0400

    modified wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,49 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+	const horizontalJoin = letters.map((ls) => ls.join(""));
+  let wordPresent = false;
+
+	for (l of horizontalJoin) {
+		if (l.includes(word)) {
+			wordPresent = true;
+		} else {
+			}
+		}
+
+    if (wordPresent === false) {
+    let vertical = verticalJoin(letters);
+
+			for (item of vertical) {
+
+				if (vertical.includes(word)) {
+					wordPresent = true;
+				} else {
+					wordPresent = false;
+				}
+      }
+	}
+    console.log(wordPresent);
+  return wordPresent
+
+};
+
+const verticalJoin = (nestedArr) => {
+	let newArray = [];
+	let arraysLength = nestedArr[0].length;
+
+	for (let a = 0; a < arraysLength; a++) {
+		newArray.push([]);
+	}
+	for (let outer = 0; outer < nestedArr.length; outer++) {
+		for (let inner = 0; inner < nestedArr[outer].length; inner++) {
+			newArray[inner][outer] = nestedArr[outer][inner];
+		}
+	}
+	return newArray;
+};
+
+module.exports = wordSearch;
+
+//Pair programmed with @Satya Sindhu
+
+//JoshuaHaughton
